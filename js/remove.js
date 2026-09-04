5d9d2d805047ca162a970551dd5fd05ceff8ce00
Zelenskyi_Bohdan <zel.bohd@gmail.com>
2026-09-04 11:47:16 +0300

    added delete button functionality: remove.js

--- js/remove.js
+++ js/remove.js
@@ -1,2 +1,13 @@
 'use strict'
 
+export default function removeCard(evt) {
+    const button = evt.target.closest('.remove-btn');
+
+    if (!button) {
+        return;
+    }
+
+    const card = button.closest('.extensions-card');
+
+    card.remove();
+}
--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 
 import data from "./js/data.js";
 import renderMarkup from "./js/render.js";
-import { changeTheme, initTheme } from "./js/theme.js"
-import {getActiveButton, filterElements} from "./js/filter.js"
+import { changeTheme, initTheme } from "./js/theme.js";
+import { getActiveButton, filterElements } from "./js/filter.js";
+import removeCard from "./js/remove.js";
 
 // Theme check
 
@@ -11,21 +12,21 @@ const themeBtn = document.querySelector('.theme-btn');
 
 initTheme();
 
+// Theme change
+
 themeBtn.addEventListener('click', changeTheme);
 
 // List render
 
 const extensions = document.querySelector('.extensions');
-
 extensions.innerHTML = renderMarkup(data);
 
-// Theme change
+// Remove element
 
-themeBtn.addEventListener('click', changeTheme);
+extensions.addEventListener('click', removeCard);
 
 // Filter elements
 
 const filterButtonsContainer = document.querySelector('.extensions-section-btns');
 filterButtonsContainer.addEventListener('click', getActiveButton);
 filterButtonsContainer.addEventListener('click', filterElements);
-
